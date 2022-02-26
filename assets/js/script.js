e3aa8d950ba7c1ba454774eeaa164a98b4f1d208
Neo <fullstackneo@gmail.com>
2022-02-26 00:37:02 -0700

    add weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,73 @@
 var searchHistory = Array[8];
 searchHistory = ["Los Angeles", "New York", "Chicago", "Las Vegas", "Salt Lake City", "San Antonio", "Phoenix", "Houston"];
 
+// display current weather
+function displayCurrent(data) {
+  // console.log(currentData);
+  $(".today ul span").each(function (index, el) {
+    $(el).text(data[index]);
+  });
+
+  // change the uv background
+  var uvlevel = data[3];
+  if (uvlevel < 3) {
+    $("#uvIndex").addClass("favorable");
+  } else if (uvlevel <= 7) {
+    $("#uvIndex").addClass("moderate");
+  } else if (uvlevel > 7) {
+    $("#uvIndex").addClass("severe");
+  }
+}
+
+//display future 5 day weather
+function displayFuture(data) {
+  console.log(data);
+
+  $(".weather-row>div").each(function (i, el) {
+    $(el).find("li:nth-child(1) span").text(data[i].temp.day);
+    $(el).find("li:nth-child(2) span").text(data[i].wind_speed);
+    $(el).find("li:nth-child(3) span").text(data[i].humidity);
+  });
+}
+
+// get weather info
+function getWeather(lat, lon) {
+  fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=07fbc9932f3a4d5f19df3aa5907fbbb2")
+    .then((response) => response.json())
+    .then((result) => {
+      // display current weather
+      var currentData = [result.current.temp, result.current.wind_speed, result.current.humidity, result.current.uvi.toFixed(2)];
+      displayCurrent(currentData);
+      //display future weather
+      var futureData = [result.daily[0], result.daily[1], result.daily[2], result.daily[3], result.daily[4]];
+      displayFuture(futureData);
+    })
+    .catch((error) => console.log("error", error));
+}
+
 // show the cities in history
 $(".search-history li").each(function (index, el) {
   $(el).text(searchHistory[index]);
 });
 
+// click search button and display result
 $("#search-form").on("submit", formDataHandler);
 
 function formDataHandler(e) {
   e.preventDefault();
   var city = $(this).find("input").val().trim();
+
+  // city default value is San Diego
   if (!city) {
     city = "San Diego";
   }
   // get city's lat and lon info
   fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" + city + "&key=AIzaSyCueXEoU9lnKGoZ8uawRHGyV8tjNV9C_Sg")
-    .then((response) => response.text())
+    .then((response) => response.json())
     .then((result) => {
-      console.log(result);
+      var lat = result.results[0].geometry.location.lat;
+      var lon = result.results[0].geometry.location.lng;
+      getWeather(lat, lon);
     })
     .catch((error) => console.log("error", error));
 }
