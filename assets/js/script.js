b6e1d2e2ba70ddadf9aea87eee023f8652c786af
Neo <fullstackneo@gmail.com>
2022-02-26 14:26:18 -0700

    add date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,22 @@
-
+// return future date according to tomorrow(i=1), the day after tomorrow(i=2) ,etc
+function displayDate(i) {
+  var date = new Date();
+  date = date.setDate(date.getDate() + i);
+  date = new Date(date);
+  return  date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear() ;
+}
 
 var searchHistory = Array[8];
 searchHistory = ["Los Angeles", "New York", "Chicago", "Las Vegas", "Salt Lake City", "San Antonio", "Phoenix", "Houston"];
 
 // display current weather
-function displayCurrent(data) {
+function displayCurrent(data, city) {
+  // display city name
+  $(".today h3").html(city + " <span></span><i>weather icon</i>");
+
+  // display current date
+  $(".today h3 span").text("("+displayDate(0)+")");
+
   // console.log(currentData);
   $(".today ul span").each(function (index, el) {
     $(el).text(data[index]);
@@ -23,6 +35,10 @@ function displayCurrent(data) {
 
 //display future day weather
 function displayFuture(futureData, i) {
+  // display date
+  $(".weather-row").children("div").eq(i).find("p").text(displayDate(i+1));
+
+  // display weather data
   $(".weather-row>div")
     .eq(i)
     .find("span")
@@ -32,13 +48,13 @@ function displayFuture(futureData, i) {
 }
 
 // get weather info
-function getWeather(lat, lon) {
+function getWeather(lat, lon, city) {
   fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=07fbc9932f3a4d5f19df3aa5907fbbb2")
     .then((response) => response.json())
     .then((result) => {
       // display current weather
       var currentData = [result.current.temp, result.current.wind_speed, result.current.humidity, result.current.uvi.toFixed(2)];
-      displayCurrent(currentData);
+      displayCurrent(currentData, city);
       //display future weather
       for (let i = 0; i < 5; i++) {
         var futureData = [result.daily[i].temp.day, result.daily[i].wind_speed, result.daily[i].humidity];
@@ -70,7 +86,7 @@ function formDataHandler(e) {
     .then((result) => {
       var lat = result.results[0].geometry.location.lat;
       var lon = result.results[0].geometry.location.lng;
-      getWeather(lat, lon);
+      getWeather(lat, lon, city);
     })
     .catch((error) => console.log("error", error));
 }
