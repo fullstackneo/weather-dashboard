99819916c898318a23de7b43286be3d2e25df20b
Neo <fullstackneo@gmail.com>
2022-02-25 22:32:38 -0700

    add geocoding api and get geo info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
 var searchHistory = Array[8];
 searchHistory = ["Los Angeles", "New York", "Chicago", "Las Vegas", "Salt Lake City", "San Antonio", "Phoenix", "Houston"];
-console.log(searchHistory);
 
+// show the cities in history
 $(".search-history li").each(function (index, el) {
   $(el).text(searchHistory[index]);
 });
 
-fetch("https://maps.googleapis.com/maps/api/geocode/json?address=san+diegoA&key=AIzaSyCueXEoU9lnKGoZ8uawRHGyV8tjNV9C_Sg")
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.log("error", error));
+$("#search-form").on("submit", formDataHandler);
+
+function formDataHandler(e) {
+  e.preventDefault();
+  var city = $(this).find("input").val().trim();
+  if (!city) {
+    city = "San Diego";
+  }
+  // get city's lat and lon info
+  fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" + city + "&key=AIzaSyCueXEoU9lnKGoZ8uawRHGyV8tjNV9C_Sg")
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => console.log("error", error));
+}
