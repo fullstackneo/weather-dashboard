65755358c3e6ebe530d172596b5ab5cd08ea0403
Neo <fullstackneo@gmail.com>
2022-02-25 22:17:54 -0700

    add cities in history column

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
-var requestOptions = {
-  method: "GET",
-  redirect: "follow",
-};
+var searchHistory = Array[8];
+searchHistory = ["Los Angeles", "New York", "Chicago", "Las Vegas", "Salt Lake City", "San Antonio", "Phoenix", "Houston"];
+console.log(searchHistory);
 
-fetch("https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=0udE9V2GRj3cRahVJ120KpyLcjQ4YXlVgLpPg4RQ", requestOptions)
+$(".search-history li").each(function (index, el) {
+  $(el).text(searchHistory[index]);
+});
+
+fetch("https://maps.googleapis.com/maps/api/geocode/json?address=san+diegoA&key=AIzaSyCueXEoU9lnKGoZ8uawRHGyV8tjNV9C_Sg")
   .then((response) => response.text())
   .then((result) => console.log(result))
   .catch((error) => console.log("error", error));
